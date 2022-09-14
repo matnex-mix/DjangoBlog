d559205c89e1cbc65271b61ab65a8ccccd445e90
Matnex Mix <41128554+matnex-mix@users.noreply.github.com>
2022-09-14 07:30:34 +0100

    added the blog site to django

--- djangoblog/settings.py
+++ djangoblog/settings.py
@@ -36,7 +36,7 @@ TESTING = len(sys.argv) > 1 and sys.argv[1] == 'test'
 # ALLOWED_HOSTS = []
 ALLOWED_HOSTS = ['*']
 # django 4.0新增配置
-# CSRF_TRUSTED_ORIGINS = ['http://example.com']
+CSRF_TRUSTED_ORIGINS = ['https://blog.rentout.app']
 # Application definition
 
 
